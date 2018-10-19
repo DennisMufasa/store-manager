df1697e22a6b24bea5b7fa887e8f3f69fa6438e8
DennisMufasa <denny.muasa@gmail.com>
2018-10-19 16:45:12 +0300

    [Feature 161342561] admin add a new attendant

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,7 +1,12 @@
 """API endpoints"""
 # third-party imports
-from flask import jsonify, request, make_response, session
+from flask import jsonify, request, make_response, session, redirect, url_for
 from . import v1_bp
+# local imports
+from . import models
+# create instances of objects
+user = models.User()
+sale = models.Sale()
 # routes
 @v1_bp.route('/products', methods=['GET', 'POST'])
 def get_products():
@@ -18,6 +23,16 @@ def get_one_sale(saleId):
 @v1_bp.route('/login', methods=['GET', 'POST'])
 def login():
     pass
-@v1_bp.route('/signup')
+@v1_bp.route('/signup', methods=['POST'])
 def signup():
-    pass
+    """Admin can add a new attendant"""
+    if not session.get("logged_in"):
+        return redirect(url_for('login'))
+    if session["username"] != "admin":
+        return make_response(jsonify({
+            "Message": "You are not an admin!"
+        }))
+    request_data = request.get_json()
+    return make_response(jsonify({
+        "Message": user.add_user(request_data)
+    }))
