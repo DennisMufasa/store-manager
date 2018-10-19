ed921ce26e09d75e856b8a95ec70051b324525c9
DennisMufasa <denny.muasa@gmail.com>
2018-10-19 16:25:06 +0300

    [Feature 161342942] edit category endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,7 +1,12 @@
 """API endpoints"""
 # third-party imports
-from flask import jsonify, request, make_response, session
+from flask import jsonify, request, make_response, session, redirect, url_for
 from . import v1_bp
+# local imports
+from . import models
+# create instances of objects
+user = models.User()
+sale = models.Sale()
 # routes
 @v1_bp.route('/products', methods=['GET', 'POST'])
 def get_products():
@@ -21,3 +26,15 @@ def login():
 @v1_bp.route('/signup')
 def signup():
     pass
+@v1_bp.route('/edit/category', methods=['PUT'])
+def edit_category():
+    """Admin edit a product category"""
+    if not session.get("logged_in"):
+        return redirect(url_for('login'), code=302)
+    if session["username"] != "admin":
+        return "you are not an admin!"
+    request_data = request.get_json()
+    return make_response(jsonify({
+        "Message": sale.edit_category(request_data)
+    }), 202)
+    
