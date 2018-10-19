4a05adf89d6c9f4ca8a61d8649cb3be1a21efe94
DennisMufasa <denny.muasa@gmail.com>
2018-10-19 14:18:11 +0300

    [Feature #161223547]get a specific order endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,14 +1,24 @@
 """API endpoints"""
 # third-party imports
-from flask import jsonify, request, make_response, session
+from flask import jsonify, request, make_response, session, redirect, url_for
 from . import v1_bp
+# local imports
+from . import models
+# create instances of objects
+user = models.User()
+sale = models.Sale()
 # routes
 @v1_bp.route('/products', methods=['GET', 'POST'])
 def get_products():
     pass
 @v1_bp.route('/products/<productId>')
 def get_one_product(productId):
-    pass
+    """Fetch a specific product using its id"""
+    if not session.get("logged_in"):
+        return redirect(url_for('login'), code=302)
+    return make_response(jsonify({
+        "Message": sale.get_one_product(productId)
+    }), 200)
 @v1_bp.route('/sales', methods=['GET', 'POST'])
 def get_sales():
     pass
