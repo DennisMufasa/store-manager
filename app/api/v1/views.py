6f899ecc3915c0ecbd405521ab6a3680c9465807
DennisMufasa <denny.muasa@gmail.com>
2018-10-19 10:24:51 +0300

    [Feature #161222007] write products API endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,11 +1,25 @@
 """API endpoints"""
 # third-party imports
-from flask import jsonify, request, make_response, session
+from flask import jsonify, request, make_response, session, url_for, redirect
 from . import v1_bp
+# local imports
+from .models import *
 # routes
 @v1_bp.route('/products', methods=['GET', 'POST'])
 def get_products():
-    pass
+    """get all products and add a new product"""
+    if not session.get("logged_in"):
+        return redirect(url_for('login'), code=302)
+    if request.method == 'POST':
+        request_data = request.get_json()
+        if not request_data:
+            return make_response(jsonify({"Message":"Enter Product Details!"}))
+        sale = Sale(request_data["name"],
+        request_data["category"],
+        request_data["quantity"],
+        request_data["unit_cost"])
+        return make_response(jsonify({"Message": sale.add_product()}))
+    return make_response(jsonify({"Message": sale.get_products()}))
 @v1_bp.route('/products/<productId>')
 def get_one_product(productId):
     pass
