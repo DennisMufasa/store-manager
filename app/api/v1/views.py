4e2b6e971651e48e0facc698d2d86046ecf074da
DennisMufasa <denny.muasa@gmail.com>
2018-10-19 16:35:14 +0300

    [Feature 161342630] edit product unit cost endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,7 +1,12 @@
 """API endpoints"""
 # third-party imports
-from flask import jsonify, request, make_response, session
+from flask import jsonify, request, make_response, session, redirect, url_for
 from . import v1_bp
+# local imports
+from . import models
+# create instances of objects
+user = models.User()
+sale = models.Sale()
 # routes
 @v1_bp.route('/products', methods=['GET', 'POST'])
 def get_products():
@@ -21,3 +26,16 @@ def login():
 @v1_bp.route('/signup')
 def signup():
     pass
+@v1_bp.route('/edit/cost', methods=['PUT'])
+def edit_cost():
+    """Admins edit a product's unit cost"""
+    if not session.get("logged_in"):
+        return redirect(url_for('/login'), code=302)
+    if session["username"] != "admin":
+        return make_response(jsonify({
+            "Message": "you are not an admin"
+        }), 401)
+    request_data = request.get_json()
+    return make_response(jsonify({
+        "Message": sale.edit_cost(request_data)
+    }), 202)
