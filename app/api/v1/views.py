771435e7e9ee89ec11eeb7b0b83e7aaba3fede94
DennisMufasa <denny.muasa@gmail.com>
2018-10-19 13:43:46 +0300

    [Feature 161224860]endpoint to log in users to their accounts

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -2,6 +2,11 @@
 # third-party imports
 from flask import jsonify, request, make_response, session
 from . import v1_bp
+# local imports
+from . import models
+# create instances of objects
+user = models.User()
+sale = models.Sale()
 # routes
 @v1_bp.route('/products', methods=['GET', 'POST'])
 def get_products():
@@ -15,9 +20,23 @@ def get_sales():
 @v1_bp.route('/sales/<saleId>')
 def get_one_sale(saleId):
     pass
-@v1_bp.route('/login', methods=['GET', 'POST'])
+@v1_bp.route('/login', methods=['POST'])
 def login():
-    pass
+    """Login users into their accounts"""
+    if not session.get("logged_in"):
+        request_data = request.get_json()
+        if user.validate_user(request_data) == "Login Successful!":
+            session["logged_in"] = True
+            session["username"] = request_data["username"]
+            return make_response(jsonify({
+                "Message": "Login Successfull!"
+            }), 200)
+        return make_response(jsonify({
+            "Message": "Log in failed! Check your credentials!"
+        }))
+    return make_response(jsonify({
+        "Message": "You are already logged in!"
+    }))
 @v1_bp.route('/signup')
 def signup():
     pass
